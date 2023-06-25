0affaf3194697945bee0969f20e29097d388d3d6
maamokun <maamo-kun@outlook.com>
2023-06-26 01:05:21 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `MikanDev's Skill Issue Log`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `MikanDev`,
+      summary: `Person with skill issue`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
